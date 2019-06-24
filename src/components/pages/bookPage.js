680e05732a2bbf4102c006c0d07872aa59fb4ff2
Mikhail <48489639+MikhailKey@users.noreply.github.com>
2019-06-24 18:19:37 +0400

    fixed

--- src/components/pages/bookPage.js
+++ src/components/pages/bookPage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ItemList from '../itemList';
 import ItemDetails, {Field} from '../itemDetails';
 import ErrorMessage from '../errorMessage';
-import gotService from '../services/gotService';
+import gotService from '../../services/gotService';
 import RowBlock from '../RowBlock';
 
 
--- src/components/pages/housePage.js
+++ src/components/pages/housePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ItemList from '../../itemList';
+import ItemList from '../itemList';
 import ItemDetails, {Field} from '../itemDetails';
 import ErrorMessage from '../errorMessage';
 import gotService from '../../services/gotService';
